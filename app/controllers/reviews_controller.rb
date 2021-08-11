02795caa7291ee4155767741e596c75444ab99a5
Quentin Peschard <qpeschard49@gmail.com>
2021-08-11 14:49:57 +0200

    finished exercise

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,11 +12,11 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
     if @review.valid?
+      @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render 'new'
+      render 'restaurants/show'
     end
   end
 
